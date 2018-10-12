066882ef2939d3f7a3c355be1f87894f3dcc0364
nenerbener <mceachern.marc@gmail.com>
2018-10-12 03:28:20 -1000

    GroupOrders initially working

--- grouporders1/src/com/nenerbener/GroupOrders.java
+++ grouporders1/src/com/nenerbener/GroupOrders.java
@@ -7,52 +7,98 @@ public class GroupOrders {
 	// driver
 	public static void main (String[] args) {
 
-		String item1 = "A";
-		String item2 = "B";
-		String item3 = "C";
-		String item4 = "D";
-//		String[] items = new String[4];
-//		items[0]=item1;
-//		items[1]=item2;
-//		items[2]=item3;
-//		items[3]=item4;
+		Item itemA = new Item("A");
+		Item itemB = new Item("B");
+		Item itemC = new Item("C");
+		Item itemD = new Item("D");
+		Item itemE = new Item("E");
 		
-		List<String> itl1 = new ArrayList<String>();
-		itl1.add(item1);
-		itl1.add(item2);
-		Order order1 = new Order("O1", itl1);
+		Order  order1 = new Order("O1");
+		Order  order2 = new Order("O2");
+		Order  order3 = new Order("O3");
+		Order  order4 = new Order("O4");
 		
-		List<String> itl2 = new ArrayList<String>();
-		itl2.add(item2);
-		itl2.add(item3);
-		Order order2 = new Order("O2", itl2);
+		order1.assocItemToOrder(itemA);
+		order1.assocItemToOrder(itemB);
 		
-		List<String> itl3 = new ArrayList<String>();
-		itl3.add(item3);
-		itl3.add(item4);
-		Order order3 = new Order("O3", itl3);
-		
-		List<Order> orders = new ArrayList<Order>();
-		orders.add(order1);
-		orders.add(order2);
-		orders.add(order3);
-		
-		itl1.retainAll(itl2);
-		System.out.println(itl1);
-		itl2.retainAll(itl3);
-		System.out.println(itl2);
+		order2.assocItemToOrder(itemB);
+		order2.assocItemToOrder(itemC);
 
+		order3.assocItemToOrder(itemD);
+		order3.assocItemToOrder(itemD);
+
+		order4.assocItemToOrder(itemD);
+		order4.assocItemToOrder(itemE);
+
+		Item it = itemB;
+		
+		OrderGrp ordGrp = new OrderGrp();
+		ordGrp.traverseToOrders(it,null);
+	}
+}
+
+// traverse graph from starting item and add orders and items found into a group
+class OrderGrp {
+	List<Order> orders;
+	List<Item> items;
+
+	OrderGrp() {
+		orders = new ArrayList<Order>();
+		items = new ArrayList<Item>();
+	}
+	
+	void traverseToOrders(Item it, Order currentOrder) {
+		System.out.println("it: " + it.itemName);
+//		System.out.println("traverseToOrder size: " + it.orders.size());
+		for(Order or:it.orders) {
+			Item currentItem = it;
+			if (or.equals(currentOrder)) continue;
+			traverseToItems(or, currentItem);
+		}
+	}
+
+	void traverseToItems(Order or, Item currentItem) {
+//		System.out.println("traverseToItems size: " + or.items.size());
+		for(Item it:or.items) {
+			Order currentOrder = or;
+			if (it.equals(currentItem)) continue;
+			traverseToOrders(it,currentOrder);
+		}
+	}
+}
+
+// order name and associated list of items
+class Order {
+	String orderName;
+	List<Item> items;
+
+	Order(String orderName) {
+		this.orderName = orderName;
+	}
+	
+	void assocItemToOrder(Item it) {
+		if (items ==null) {
+			items = new ArrayList<Item>();
+		}
+		items.add(it);
+		it.assocOrderToItem(this);
 	}
 	
 }
 
-// order has order number and number of items
-class Order {
-	String orderNum;
-	List<String> items;
-	Order(String orderNum, List<String> items) {
-		this.orderNum = orderNum;
-		this.items = items;
+// item name and associated list of orders
+class Item {
+	String itemName;
+	List<Order> orders;
+
+	Item (String itemName) {
+		this.itemName = itemName;
 	}
-	
+
+	void assocOrderToItem(Order or) {
+		if (orders == null) {
+			orders = new ArrayList<Order>();
+		}
+		orders.add(or);
+	}
 }
