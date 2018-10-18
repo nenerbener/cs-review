32f3d3261de5bafed77ec47b36730b0542084759
nenerbener <mceachern.marc@gmail.com>
2018-10-18 10:56:01 -1000

    modified closestlocation to abstractqueue reference and added toString for PQObject

--- closestlocations/src/com/nenerbener/ClosestLocations.java
+++ closestlocations/src/com/nenerbener/ClosestLocations.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Comparator;
-
+import java.util.AbstractQueue; //actually a concrete base class for many queues ( 
+//ArrayBlockingQueue, ConcurrentLinkedQueue, DelayQueue, LinkedBlockingDeque
+//LinkedBlockingQueue, LinkedTransferQueue, PriorityBlockingQueue, PriorityQueue, SynchronousQueue
 
 public class ClosestLocations {
 
@@ -16,20 +17,21 @@ public class ClosestLocations {
 			   {{new Integer(3),new Integer(3)},
 				{new Integer(2),new Integer(2)},
 				{new Integer(1),new Integer(1)}};
+		
+		System.out.println(Arrays.toString(intArray[0]));
+		System.out.println(Arrays.toString(intArray[1]));
+		System.out.println(Arrays.toString(intArray[2]));
+		
 		List<Integer[]> list = Arrays.asList(intArray);
-		System.out.println("list: " + intArray);
 		List<PQObject> results = cl.getNearestLocations(list);
-		for (PQObject obj:results) {
-			System.out.print("r: " + obj.getI() + " ");
-			System.out.print("x: " + obj.getList()[0] + " ");
-			System.out.println("y: " + obj.getList()[1]);
-		}
+		System.out.println(results);
 	}
 
 	List<PQObject> getNearestLocations(List<Integer[]> list)
 	{
 		List<PQObject> result = new ArrayList<>();
-		PriorityQueue<PQObject> pq = new PriorityQueue<>(100, new Comparator<PQObject>() {
+//		PriorityQueue<PQObject> pq = new PriorityQueue<>(100, new Comparator<PQObject>() {
+		AbstractQueue<PQObject> pq = new PriorityQueue<>(100, new Comparator<PQObject>() {
 			@Override
 			public int compare(PQObject a1, PQObject a2) {
 				return Integer.compare(a1.getI().intValue(), a2.getI().intValue());
@@ -61,10 +63,15 @@ class PQObject implements PQElement<Integer,Integer[]>{
 	public Integer getI() {
 		return i;
 	}
+	@Override
+	public String toString() {
+		return "r:" + this.i + " row:" + this.getList()[0] + " col:" + this.getList()[1];
+	}
 }
 
-interface PQElement<Integer,T2> {
-	void set(Integer i, T2 ta); //add
+interface PQElement<T1,T2> {
+	void set(T1 i, T2 ta); //add
 	T2 getList(); //get the object with the integer
-	Integer getI(); //get the object with the integer
+	T1 getI(); //get the object with the integer
+	String toString(); //get the object with the integer
 }
