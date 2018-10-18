97a2cafcb044d047efe0347031bbd438c5dbcd34
nenerbener <mceachern.marc@gmail.com>
2018-10-18 12:47:01 -1000

    little mods to closestlocation

--- closestlocations/src/com/nenerbener/ClosestLocations.java
+++ closestlocations/src/com/nenerbener/ClosestLocations.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.AbstractQueue; //actually a concrete base class for many queues ( 
+import java.util.AbstractQueue; 
+//AbstractQueue is concrete base class for many queues ( 
 //ArrayBlockingQueue, ConcurrentLinkedQueue, DelayQueue, LinkedBlockingDeque
 //LinkedBlockingQueue, LinkedTransferQueue, PriorityBlockingQueue, PriorityQueue, SynchronousQueue
 
@@ -15,8 +16,8 @@ public class ClosestLocations {
 		ClosestLocations cl = new ClosestLocations();
 		Integer[][] intArray = 
 			   {{new Integer(3),new Integer(3)},
-				{new Integer(2),new Integer(2)},
-				{new Integer(1),new Integer(1)}};
+				{new Integer(1),new Integer(1)},
+				{new Integer(2),new Integer(2)}};
 		
 		System.out.println(Arrays.toString(intArray[0]));
 		System.out.println(Arrays.toString(intArray[1]));
@@ -30,13 +31,23 @@ public class ClosestLocations {
 	List<PQObject> getNearestLocations(List<Integer[]> list)
 	{
 		List<PQObject> result = new ArrayList<>();
-//		PriorityQueue<PQObject> pq = new PriorityQueue<>(100, new Comparator<PQObject>() {
-		AbstractQueue<PQObject> pq = new PriorityQueue<>(100, new Comparator<PQObject>() {
+		QueueFactory qf = new QueueFactory();
+		Comparator<PQObject> cpq = new Comparator<PQObject>() {
 			@Override
-			public int compare(PQObject a1, PQObject a2) {
+			public int compare(PQObject a2, PQObject a1) {
 				return Integer.compare(a1.getI().intValue(), a2.getI().intValue());
 			}
-		});	
+		};
+
+		AbstractQueue<PQObject> pq = (PriorityQueue<PQObject>)qf.getQueue("PriorityQueue",100,cpq);
+
+//		AbstractQueue<PQObject> pq = new PriorityQueue<>(100, new Comparator<PQObject>() {
+//			@Override
+//			public int compare(PQObject a1, PQObject a2) {
+//				return Integer.compare(a1.getI().intValue(), a2.getI().intValue());
+//			}
+//		});	
+
 		for (Integer[] elem: list) {
 			PQObject obj = new PQObject(); 
 			obj.set(elem[0]*elem[0]+elem[1]*elem[1], elem);
@@ -50,6 +61,7 @@ public class ClosestLocations {
 class PQObject implements PQElement<Integer,Integer[]>{
 	Integer i;
 	Integer[] li;
+
 	@Override
 	public void set(Integer i, Integer[] ta) {
 		this.i = i;
@@ -65,7 +77,7 @@ class PQObject implements PQElement<Integer,Integer[]>{
 	}
 	@Override
 	public String toString() {
-		return "r:" + this.i + " row:" + this.getList()[0] + " col:" + this.getList()[1];
+		return "r2:" + this.i + " row:" + this.getList()[0] + " col:" + this.getList()[1];
 	}
 }
 
@@ -74,4 +86,25 @@ interface PQElement<T1,T2> {
 	T2 getList(); //get the object with the integer
 	T1 getI(); //get the object with the integer
 	String toString(); //get the object with the integer
+}
+
+class QueueFactory implements QueueFactoryInterface<PQObject> {
+	QueueFactory(){};
+	
+	@Override
+	public AbstractQueue<PQObject> getQueue(String queueType) {
+		if (queueType.equals("PriorityQueue")) return new PriorityQueue<PQObject>();
+		else return null;
+	}
+
+	@Override
+	public AbstractQueue<PQObject> getQueue(String queueType, int size, Comparator<PQObject> c) {
+		if (queueType.equals("PriorityQueue")) return new PriorityQueue<PQObject>(100, c);
+		else return null;
+	}
+}
+
+interface QueueFactoryInterface<QT> {
+	AbstractQueue<QT> getQueue(String queueType);
+	AbstractQueue<QT> getQueue(String queueType, int size, Comparator<QT> c);
 }
