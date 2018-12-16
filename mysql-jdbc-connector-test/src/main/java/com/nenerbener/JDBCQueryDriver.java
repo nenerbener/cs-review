e7041b04e17fd68a2766311057ebf9ca0240d408
nenerbener <mceachern.marc@gmail.com>
2018-12-16 10:09:11 -1000

    -

--- mysql-jdbc-connector-test/src/main/java/com/nenerbener/JDBCQueryDriver.java
+++ mysql-jdbc-connector-test/src/main/java/com/nenerbener/JDBCQueryDriver.java
@@ -5,8 +5,6 @@ package com.nenerbener;
 
 import java.sql.*;
 
-
-
 /**
  * A simple driver to connect to localhost:3306 school_records database and perform queries
  * @author mm
@@ -19,20 +17,45 @@ public class JDBCQueryDriver {
 	 */
 	public static void main(String[] args) throws Exception {
 		
-//	create statement
-	StringBuilder sb = new StringBuilder("SELECT students.student_id,"); 
-	sb.append(" CONCAT(students.first_name, \" \", students.last_name) AS Name,");
-	sb.append(" COUNT(absences.date) AS Absences");
-	sb.append(" FROM students LEFT JOIN absences");
-	sb.append(" ON students.student_id = absences.student_id");
-	sb.append(" GROUP BY students.student_id;");
-	String sbFinal = sb.toString();
-//		Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc.mysql://localhost:3306/school_records", "root", "q");
+		Connection con = null;
+
+		//Register driver class
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} 
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+
+		//	create statement
+		StringBuilder sb = new StringBuilder("SELECT students.student_id,"); 
+		sb.append(" CONCAT(students.first_name, \" \", students.last_name) AS Name,");
+		sb.append(" COUNT(absences.date) AS Absences");
+		sb.append(" FROM students LEFT JOIN absences");
+		sb.append(" ON students.student_id = absences.student_id");
+		sb.append(" GROUP BY students.student_id;");
+		String sbFinal = sb.toString();
+		
+		// create a connection
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/school_records?user=root&password=q");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		// perform query and return result set
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery(sbFinal);
+
+		// output
+		while (rs.next()) {
+			System.out.print("student_id: " + rs.getInt("students.student_id"));
+			System.out.print("  student name: " + rs.getString("Name"));
+			System.out.print("  absences: " + rs.getInt("Absences"));
+			System.out.println();
+		}
+		
 		System.exit(0);
 		
 	}
-
 }
