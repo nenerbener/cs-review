510e5d964546bccf61e7cd707b7e048658f94cb7
nenerbener <mceachern.marc@gmail.com>
2019-01-13 11:46:51 -1000

    -

--- ng2-demo3/src/app/app.component.ts
+++ ng2-demo3/src/app/app.component.ts
@@ -1,28 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+    serverElements = [{ type: 'server', name: 'TestServer', content: 'Just a test' }];
+    onServerAdded(serverData: { serverName: string, serverContent: string }) {
+        this.serverElements.push({
+            type: 'server',
+            name: serverData.serverName,
+            content: serverData.serverContent
+        });
+    }
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+    onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+        this.serverElements.push({
+            type: 'blueprint',
+            name: blueprintData.serverName,
+            content: blueprintData.serverContent
+        });
+    }
 }
