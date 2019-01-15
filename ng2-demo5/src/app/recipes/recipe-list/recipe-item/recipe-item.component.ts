4218aef1ba81b9075d2d9226523ccdf06b2538b2
nenerbener <mceachern.marc@gmail.com>
2019-01-14 17:36:47 -1000

    -

--- ng2-demo5/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ ng2-demo5/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+    selector: 'app-recipe-item',
+    templateUrl: './recipe-item.component.html',
+    styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
 
-  constructor() { }
+@Output() recipeSelected = new EventEmitter<void>();
 
-  ngOnInit() {
-  }
+    @Input() recipe: Recipe;
+    constructor() { }
 
+    ngOnInit() {
+    }
+ 
+    onClick() {
+this.recipeSelected.emit();
+    }
 }
